2081382aeef453ce6430b199624fa058036ff1ce
maxpe3447 <121520054+mentorAC@users.noreply.github.com>
2023-02-01 20:05:35 +0200

    add les 26 27

---
+++ Les26/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Text;
+
+#region File
+//var lines = new string[]
+//{
+//    "tom", "bob", "mark"
+//};
+//File.WriteAllLines("file.txt", lines);
+//var readLines = File.ReadAllLines("file.txt");
+//Console.WriteLine(string.Join("\n", readLines));
+#endregion
+
+#region FileStream
+//FileStream
+//var file  = new FileStream("fileStream.txt", FileMode.OpenOrCreate);
+//file.Close();
+
+//using (var file = new FileStream("file.txt", FileMode.OpenOrCreate))
+//{
+//    Console.WriteLine(file.Length);
+//    Console.WriteLine(file.Position);
+//    Console.WriteLine(file.Name);
+
+//    file.Seek(3, SeekOrigin.Begin);
+//    Console.WriteLine(file.Position);
+
+//    file.Seek(2, SeekOrigin.Current);
+//    Console.WriteLine(file.Position);
+
+//}
+//Flush 
+#endregion
+
+#region FileStream-Write-Read
+
+string fileName = $"{nameof(fileName)}.txt";
+string text = "hello filestream!!!";
+
+using (var file = new FileStream(fileName, FileMode.OpenOrCreate))
+{
+    var bytes = Encoding.Default.GetBytes(text);
+
+    file.Write(bytes, 0, bytes.Length);
+    file.WriteByte(63);
+    //file.Flush();
+    Console.WriteLine("Text wrote");
+
+}
+
+using (var file = File.OpenRead(fileName))
+{
+    file.Seek(6, SeekOrigin.Begin);
+    byte[] bytes = new byte[10];
+    file.Read(bytes, 0, bytes.Length);
+    string readText = Encoding.Default.GetString(bytes);
+    //Console.WriteLine(string.Join(" ", bytes));
+    Console.WriteLine(readText);
+}
+
+#endregion
---
+++ Les27/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Text;
+
+namespace Les27
+{
+    class GameConfig
+    {
+        public int Score { get; set; }
+        public string Player { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Writer-Reader
+
+            //StreamReader
+
+            //StreamWriter
+            //StreamWriter(string path)
+            //StreamWriter(string path, bool append)
+            //string filename = "my_file.txt";
+
+            #region Writer
+
+            //using (var sw = new StreamWriter(filename, false, Encoding.Default))
+            //{
+            //    sw.WriteLine("Hi!");
+            //    sw.WriteLine("51");
+            //    sw.Write(new string('@', 50));
+            //}
+
+            //using (var sw = new StreamWriter(filename, true))
+            //{
+            //    //Flush
+            //    sw.WriteLine();
+            //    sw.WriteLine("5.21");
+            //    sw.Write(new string('#', 10));
+            //}
+            #endregion
+
+            #region Reader
+
+            //StreamReader(path)
+            //StreamReader(path, System.Text.Encoding encoding)
+
+            //using (var sr = new StreamReader(filename, Encoding.Default))
+            //{
+            //    var text = sr.ReadToEnd();
+            //    //var text = sr.ReadLine();
+            //    //text = sr.ReadLine();
+            //    //Console.WriteLine(text);
+            //    ///////////////////////////////////
+            //    //string? text;
+
+            //    //while ((text = sr.ReadLine()) != null)
+            //    //{
+            //    //    Console.WriteLine(text);
+            //    //}
+            //    ///////////////////////////////////
+            //    //int char_;
+            //    //while ((char_ = sr.Peek()) != -1)
+            //    //{
+            //    //    Console.Write(char_);
+            //    //}
+
+
+            //}
+
+            #endregion
+
+            #endregion
+
+            string path = "file.dat";
+            string game_path = "game.dat";
+
+            GameConfig game = new GameConfig()
+            {
+                Score = 51,
+                Player = "Max"
+            };
+
+            //BinaryWriter()
+            using (var bw = new BinaryWriter(File.Open(game_path, FileMode.OpenOrCreate),Encoding.UTF8))
+            {
+                //bw.Write("Hello binary file!");
+                //bw.Write(100);
+
+                bw.Write(game.Score);
+                bw.Write("game.Player");
+            }
+
+            using (var br = new BinaryReader(File.Open(game_path, FileMode.Open), Encoding.UTF8))
+            {
+                int score  = br.ReadInt32();
+                Console.WriteLine(score);
+
+                var size = br.ReadByte();
+                Console.WriteLine(size);
+                //string name = br.ReadString();
+                //Console.WriteLine(name);
+            }
+        }
+    }
+}
